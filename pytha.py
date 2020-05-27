ae5fc9e4bc3325b36326593a796537a2c0d06930
Arthur Richard <arthur.richard2299@gmail.com>
2020-05-27 13:37:51 +0200

    retirer assert

--- pytha.py
+++ pytha.py
@@ -3,7 +3,6 @@ def pythagore(hype, cote_b, cote_c):
     assert isinstance(hype, float), "Mauvais typage pour a"
     assert isinstance(cote_b, float), "Mauvais typage pour b"
     assert isinstance(cote_c, float), "Mauvais typage pour c"
-    assert hype > cote_b and hype > cote_c, "Hypoténuse mauvais"
 
     somme_carre = (cote_b ** 2 + cote_c ** 2)
     return hype ** 2 == somme_carre
